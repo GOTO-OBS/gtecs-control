38096331b015390e396d1117b116566f324359e7
Martin Dyer <martin.dyer@sheffield.ac.uk>
2015-11-09 11:03:21 +0000

    Rework follower, add more daemons

--- follower.py
+++ follower.py
@@ -11,66 +11,43 @@
 
 ### Import ###
 # Python modules
-import time
-import Pyro4
+import os, sys, commands
+import time, datetime
+import subprocess
 # TeCS modules
 import X_params as params
-import X_misc as misc
 
+def queue_info():
+    proc = subprocess.Popen('python queueX.py info', shell=True, stdout=subprocess.PIPE)
+    queue_info = proc.communicate()[0]
+    return queue_info
+    
+def cam_info():
+    proc = subprocess.Popen('python cam.py info', shell=True, stdout=subprocess.PIPE)
+    cam_info = proc.communicate()[0]
+    return cam_info
 
-def get_info():
-    MNT_DAEMON_ADDRESS=params.DAEMONS['mnt']['ADDRESS']
-    mnt=Pyro4.Proxy(MNT_DAEMON_ADDRESS)
-    #try:
-    mnt.get_info()
-    time.sleep(0.1) # Wait for it to update
-    info = mnt.report_to_UI('info')
-    
-    if info['targ_ra'] != None and info['targ_dec'] != None:
-        targ_dist = misc.ang_sep(info['tel_ra'],info['tel_dec'],info['targ_ra'],info['targ_dec'])
-    else:
-        targ_dist = None
-    
-    print '#### MOUNT INFO ####'
-    if info['status'] != 'Slewing':
-        print 'Status: %s' %info['status']
-    else:
-        print 'Status: %s (%.2f)' %(info['status'],targ_dist)
-    print '~~~~~~~'
-    print 'Telescope RA:  %.4f' %info['tel_ra']
-    print 'Telescope Dec: %.4f' %info['tel_dec']
-    if info['targ_ra'] != None:
-        print 'Target RA:     %.4f' %info['targ_ra']
-    else:
-        print 'Target RA:     NONE'
-    if info['targ_dec'] != None:
-        print 'Target Dec:    %.4f' %info['targ_dec']
-    else:
-        print 'Target Dec:    NONE'
-    if targ_dist != None:
-        print 'Target dist:   %.4f' %targ_dist
-    else:
-        print 'Target dist:   N/A'
-    print 'Telescope Alt: %.2f' %info['tel_alt']
-    print 'Telescope Az:  %.2f' %info['tel_az']
-    print 'Step size: %.2f arcsec' %info['step']
-    print '~~~~~~~'
-    print 'LST: %.2f' %info['lst']
-    print 'Hour Angle: %.2f' %info['ha']
-    print 'Tel Time: %s' %info['teltime']
-    #print 'UTC: %s' %info['ut']
-    print '~~~~~~~'
-    print 'Uptime: %.1fs' %info['uptime']
-    print 'Ping: %.3fs' %info['ping']
-    #print 'Site Long: %.2f' %info['long']
-    #print 'Site Lat: %.2f' %info['lat']
-    #print 'Site Eliv: %.2f' %info['eliv']
-    print '####################'
-    #except:
-        #print 'No response from daemon'
+def filt_info():
+    proc = subprocess.Popen('python filt.py info', shell=True, stdout=subprocess.PIPE)
+    filt_info = proc.communicate()[0]
+    return filt_info
+
+def foc_info():
+    proc = subprocess.Popen('python foc.py info', shell=True, stdout=subprocess.PIPE)
+    foc_info = proc.communicate()[0]
+    return foc_info
     
 if __name__ == '__main__':
     
     while 1:
-        get_info()
-        time.sleep(1.)
+        queue = queue_info()
+        cam = cam_info()
+        filt = filt_info()
+        foc = foc_info()
+        now = datetime.datetime.utcnow()
+        print now.strftime('%Y-%m-%d %H:%M:%S') + '\n'
+        print queue
+        print cam
+        print filt
+        print foc
+        time.sleep(0.5)
