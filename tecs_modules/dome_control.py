87ee061dbdf8addb8eede10e5e47e93aeabd48d4
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-06-27 16:06:21 +0100

    automatic python-modernize changes

--- tecs_modules/dome_control.py
+++ tecs_modules/dome_control.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+from __future__ import print_function
 #oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo#
 #                           dome_control.py                            #
 #           ~~~~~~~~~~~~~~~~~~~~~~~##~~~~~~~~~~~~~~~~~~~~~~~           #
