50764160b8ec8537edce11e37495d56847818517
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-06-27 16:44:00 +0100

    removed import of split

--- dome.py
+++ dome.py
@@ -14,7 +14,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import os, sys
-from string import split
 import readline
 import time
 import Pyro4
@@ -72,7 +71,7 @@ def halt_dome():
 # Interactive mode
 def interactive():
     while True:
-        command = split(input('dome> '))
+        command = input('dome> ').split()
         if len(command) > 0:
             if command[0] == 'q' or command[0] == 'exit':
                 return
