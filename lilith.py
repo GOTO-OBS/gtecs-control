1b8aca0681464f4ff16aba15b123add145ded5e1
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-06-30 10:41:26 +0100

    lilith must start daemons using misc.start_daemon, otherwise she'll hang if daemon output goes to console

--- lilith.py
+++ lilith.py
@@ -21,13 +21,45 @@ import Pyro4
 from tecs_modules import misc
 from tecs_modules import params
 
+
+def start_daemon(daemon_key):
+    DAEMON_PROCESS = params.DAEMONS[daemon_key]['PROCESS']
+    DAEMON_HOST = params.DAEMONS[daemon_key]['HOST']
+    if params.REDIRECT_STDOUT:
+        DAEMON_OUTPUT = params.LOG_PATH + '{}_daemon-stdout.log'.format(daemon_key)
+    else:
+        DAEMON_OUTPUT = '/dev/stdout'
+    misc.start_daemon(DAEMON_PROCESS, DAEMON_HOST, stdout=DAEMON_OUTPUT)
+
+
+def shutdown_daemon(daemon_key):
+    DAEMON_ADDRESS = params.DAEMONS[daemon_key]['ADDRESS']
+    misc.shutdown_daemon(DAEMON_ADDRESS)
+
+
+def kill_daemon(daemon_key):
+    DAEMON_PROCESS = params.DAEMONS[daemon_key]['PROCESS']
+    DAEMON_HOST = params.DAEMONS[daemon_key]['HOST']
+    misc.kill_daemon(DAEMON_PROCESS, DAEMON_HOST)
+
+
+def ping_daemon(daemon_key):
+    DAEMON_ADDRESS = params.DAEMONS[daemon_key]['ADDRESS']
+    misc.ping_daemon(DAEMON_ADDRESS)
+
+routines = dict(start=start_daemon, shutdown=shutdown_daemon,
+                kill=kill_daemon, ping=ping_daemon)
+
+
 if __name__ == '__main__':
-    if sys.argv[1] in ['start','shutdown','kill','ping']:
+    if sys.argv[1] in ['start', 'shutdown', 'kill', 'ping']:
         if len(sys.argv) > 2:
             daemons = sys.argv[2:]
         else:
-            daemons = list(params.DAEMONS.keys())
+            daemons = list(params.DAEMONS)
         for d in daemons:
-            print(d+':\t' + misc.python_command(d+'.py', sys.argv[1]))
+            print(d+':\t', end='')
+            func = routines[sys.argv[1]]
+            func(d)
     else:
         print('Valid commands: start, shutdown, kill, ping')
