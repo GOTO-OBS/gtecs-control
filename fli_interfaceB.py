6835df4348f151161502ef5cb7494ec104fb05a9
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-06-30 10:55:05 +0100

    non-ascii character in python files

--- fli_interfaceB.py
+++ fli_interfaceB.py
@@ -77,7 +77,7 @@ class FLI:
                 #self.nuc = nuc
         self.nuc = 'nuc2'
 
-        # logger object
+        # logger object
         self.logfile = logger.getLogger('fli_interfaceB', file_logging=params.FILE_LOGGING,
                                         stdout_logging=params.STDOUT_LOGGING)
 
