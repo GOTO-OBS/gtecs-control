37a0149a996a8ece0863bf7c063e95fd0ff04b76
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-06-30 09:39:57 +0100

    cleanup minor bugs in output

--- filt_daemon.py
+++ filt_daemon.py
@@ -96,7 +96,7 @@ class FiltDaemon:
                         self.serial_number[nuc][HW] = fli.get_filter_serial_number(HW)
                         self.homed[nuc][HW] = fli.get_filter_homed(HW)
                     except:
-                        self.logfile.exception('No response from fli interface on %d', nuc)
+                        self.logfile.exception('No response from fli interface on %s', nuc)
                 # save info
                 info = {}
                 for tel in list(self.tel_dict.keys()):
@@ -135,7 +135,7 @@ class FiltDaemon:
                         c = fli.set_filter_pos(new_filter_num,HW)
                         if c: self.logfile.info(c)
                     except:
-                        self.logfile.exception('No response from fli interface on %d', nuc)
+                        self.logfile.exception('No response from fli interface on %s', nuc)
                 # clear the 'active' units
                 self.active_tel = []
 
@@ -156,7 +156,7 @@ class FiltDaemon:
                         c = fli.home_filter(HW)
                         if c: self.logfile.info(c)
                     except:
-                        self.logfile.exception('No response from fli interface on %d', nuc)
+                        self.logfile.exception('No response from fli interface on %s', nuc)
                 # clear the active units
                 self.active_tel = []
 
