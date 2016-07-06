ecccf222fadf514a226ac47915c48c71a0f80c7f
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-07-06 09:21:38 +0100

    adjusting to change in module structure

--- gtecs/observing_scripts/start_of_night_1.py
+++ gtecs/observing_scripts/start_of_night_1.py
@@ -6,14 +6,21 @@ This script should perform the following simple tasks:
 * start the daemons
 * empty the persistent camera queues
 * unpause the camera daemon
+<<<<<<< 81b68339c14fe7cf121b757e77b318bea23fb2cf
 * home the filter wheel
+=======
+>>>>>>> adjusting to change in module structure
 * start the pipeline data reduction
 """
 from __future__ import absolute_import
 from __future__ import print_function
 import time
 from gtecs.tecs_modules.misc import execute_command as cmd
+<<<<<<< 81b68339c14fe7cf121b757e77b318bea23fb2cf
 
+=======
+import sys
+>>>>>>> adjusting to change in module structure
 
 def run():
     print('Start of Night Phase 1')
@@ -34,11 +41,18 @@ def run():
     time.sleep(1)
     cmd('exq resume')
 
+<<<<<<< 81b68339c14fe7cf121b757e77b318bea23fb2cf
     # home the wheels
     cmd('filt home')
 
+=======
+>>>>>>> adjusting to change in module structure
     # start the pipeline DR (TODO)
     # cmd('qsireduce start')
 
 if __name__ == "__main__":
+<<<<<<< 81b68339c14fe7cf121b757e77b318bea23fb2cf
     run()
+=======
+    run()
+>>>>>>> adjusting to change in module structure
