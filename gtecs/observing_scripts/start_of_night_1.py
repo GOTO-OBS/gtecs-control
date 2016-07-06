fd12290ea24dd736f300843f11c21d9f8710629b
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-07-06 15:36:09 +0100

    PEP8

--- gtecs/observing_scripts/start_of_night_1.py
+++ gtecs/observing_scripts/start_of_night_1.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 import time
 from gtecs.tecs_modules.misc import execute_command as cmd
-import sys
+
 
 def run():
     print('Start of Night Phase 1')
