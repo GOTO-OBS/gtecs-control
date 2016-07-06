3bcac7b6db9fb01da7ed36d933af9d46857ee4c0
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-07-06 09:21:38 +0100

    adjusting to change in module structure

---
+++ gtecs/observing_scripts/start_of_night_1.py
@@ -0,0 +1,40 @@
+"""
+Script to run the tasks for Start Night Phase 1.
+
+This script should perform the following simple tasks:
+* power on the equipment
+* start the daemons
+* empty the persistent camera queues
+* unpause the camera daemon
+* start the pipeline data reduction
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+import time
+from gtecs.tecs_modules.misc import execute_command as cmd
+import sys
+
+def run():
+    print('Start of Night Phase 1')
+
+    cmd('lilith start power')
+    cmd('power on filt')
+    cmd('power on foc')
+    cmd('power on cam')
+    cmd('power on mnt')
+    time.sleep(5)
+
+    # start the daemons
+    cmd('lilith start')
+    time.sleep(15)
+
+    # clean up persistent queue from previous night
+    cmd('exq clear')
+    time.sleep(1)
+    cmd('exq resume')
+
+    # start the pipeline DR (TODO)
+    # cmd('qsireduce start')
+
+if __name__ == "__main__":
+    run()
