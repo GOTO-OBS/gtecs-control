38eeb1a6c72f7c5e6ccccd6b9b5ec1cceb1dbed0
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-07-12 13:58:54 +0100

    setup.py correctly installs folders
    Also correctly sets permissions, but not ownership

--- setup.py
+++ setup.py
@@ -6,13 +6,25 @@ import shutil
 
 # create CONFIG_DIR if necessary and put files there
 from gtecs.tecs_modules import params
-if not os.path.exists(params.CONFIG_PATH):
-      os.mkdir(params.CONFIG_PATH)
-      os.mkdir(os.path.join(params.CONFIG_PATH, 'logs'))
-      os.mkdir(os.path.join(params.CONFIG_PATH, 'images'))
-shutil.copy('gtecs/data/conditions_flags', os.path.join(params.CONFIG_PATH, 'conditions_flags'))
-shutil.copy('gtecs/data/overrides_flags', os.path.join(params.CONFIG_PATH, 'overrides_flags'))
-shutil.copy('gtecs/data/run_number', os.path.join(params.CONFIG_PATH, 'run_number'))
+
+try:
+    if not os.path.exists(params.CONFIG_PATH):
+        os.mkdir(params.CONFIG_PATH)
+        os.chmod(params.CONFIG_PATH, 0o775)
+    logpath = os.path.join(params.CONFIG_PATH, 'logs')
+    if not os.path.exists(logpath):
+        os.mkdir(logpath)
+        os.chmod(logpath, 0o775)
+    imagepath = os.path.join(params.CONFIG_PATH, 'images')
+    if not os.path.exists(imagepath):
+        os.mkdir(imagepath)
+        os.chmod(imagepath, 0o775)
+    shutil.copy('gtecs/data/conditions_flags', os.path.join(params.CONFIG_PATH, 'conditions_flags'))
+    shutil.copy('gtecs/data/overrides_flags', os.path.join(params.CONFIG_PATH, 'overrides_flags'))
+    shutil.copy('gtecs/data/run_number', os.path.join(params.CONFIG_PATH, 'run_number'))
+except:
+    print('Failed to set up the CONFIG_PATH ', params.CONFIG_PATH,
+          'You should create this directory yourself manually')
 
 setup(name='gtecs',
       version='0.1',
