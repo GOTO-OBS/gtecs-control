a37e41354a255591e6d4d0753fdfacf68adfb2db
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-07-12 15:28:25 +0100

    moved directory creation to setup script

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
 from __future__ import print_function
 from setuptools import setup
 import glob
-import os
-import shutil
-
-# create CONFIG_DIR if necessary and put files there
-from gtecs.tecs_modules import params
-
-try:
-    if not os.path.exists(params.CONFIG_PATH):
-        os.mkdir(params.CONFIG_PATH)
-        os.chmod(params.CONFIG_PATH, 0o775)
-    logpath = os.path.join(params.CONFIG_PATH, 'logs')
-    if not os.path.exists(logpath):
-        os.mkdir(logpath)
-        os.chmod(logpath, 0o775)
-    imagepath = os.path.join(params.CONFIG_PATH, 'images')
-    if not os.path.exists(imagepath):
-        os.mkdir(imagepath)
-        os.chmod(imagepath, 0o775)
-    shutil.copy('gtecs/data/conditions_flags', os.path.join(params.CONFIG_PATH, 'conditions_flags'))
-    shutil.copy('gtecs/data/overrides_flags', os.path.join(params.CONFIG_PATH, 'overrides_flags'))
-    shutil.copy('gtecs/data/run_number', os.path.join(params.CONFIG_PATH, 'run_number'))
-except:
-    print('Failed to set up the CONFIG_PATH ', params.CONFIG_PATH,
-          'You should create this directory yourself manually')
 
 setup(name='gtecs',
       version='0.1',
@@ -34,7 +10,7 @@ setup(name='gtecs',
       author_email='martin.dyer@sheffield.ac.uk',
       packages=['gtecs', 'gtecs.tecs_modules', 'gtecs.catalogs',
                 'gtecs.daemons'],
-      package_data={'': ['data/*.ini']},
+      package_data={'': ['data/*']},
       install_requires=['Pyro4', 'astropy', 'configobj'],
       scripts=glob.glob('scripts/*'),
       include_package_data=True,
