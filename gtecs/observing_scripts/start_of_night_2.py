5ce52fb83d5a08015ec2c7d80800ec3ee0c519c7
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-08-17 22:56:16 -0300

    repeat mount info after unpark

--- gtecs/observing_scripts/start_of_night_2.py
+++ gtecs/observing_scripts/start_of_night_2.py
@@ -9,11 +9,13 @@ from __future__ import print_function
 import time
 from gtecs.tecs_modules.misc import execute_command as cmd
 
+
 def run():
     print('Start of Night Phase 2')
     cmd('mnt info')
     cmd('mnt unpark')
     time.sleep(5)
+    cmd('mnt info')
 
 if __name__ == "__main__":
     run()
