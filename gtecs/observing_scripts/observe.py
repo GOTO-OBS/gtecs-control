86c1c64c7de147f7f3412582b1c8cf8d407bed81
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-08-20 05:31:45 -0300

    just waits for mintime
    marks as completed if it reaches the end

--- gtecs/observing_scripts/observe.py
+++ gtecs/observing_scripts/observe.py
@@ -8,26 +8,37 @@ from __future__ import absolute_import
 from __future__ import print_function
 import sys
 import time
-from gtecs.tecs_modules.misc import neatCloser, python_command
+
+import numpy as np
+
+from gtecs.tecs_modules.misc import neatCloser
+from gtecs.database import markJobCompleted
 
 
 class Closer(neatCloser):
     """
-    An example class to handle shutdown.
+    A class to neatly handle shutdown requests.
+
+    We mark the job as aborted
     """
     def __init__(self, taskName, jobID):
         super().__init__(taskName)
         self.jobID = jobID
 
     def tidyUp(self):
-        print('Marking {} as cancelled'.format(self.jobID))
+        print('Received cancellation of job {}'.format(self.jobID))
 
 
 if __name__ == "__main__":
-    name = sys.argv[1]
-    duration = float(sys.argv[2])
-    # use closer class to handle interrupts
-    closer = Closer(name, name)
-    print('Running', name)
-    time.sleep(duration)
-    print('Task {} completed'.format(name))
+
+    pID = int(sys.argv[1])
+    minTime = int(sys.argv[2])
+    closer = Closer(pID, pID)
+    print('Observing pointingID: ', pID)
+
+    extra_time = np.random.uniform(10,20)
+    time.sleep(minTime + extra_time)
+
+    # hey, if we got here no-one else will mark as completed
+    markJobCompleted(pID)
+    print('Pointing {} completed'.format(pID))
