b1da00e4091d73446809ce34c590aff0c9675931
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-08-21 00:26:46 -0300

    move scope to LST+4 on first unpark

--- gtecs/observing_scripts/start_of_night_2.py
+++ gtecs/observing_scripts/start_of_night_2.py
@@ -7,13 +7,25 @@ This script should perform the following simple tasks:
 from __future__ import absolute_import
 from __future__ import print_function
 import time
+
+from astropy.time import Time
+from astropy.coordinates import Longitude
+from astropy import units as u
+
 from gtecs.tecs_modules.misc import execute_command as cmd
+from gtecs.tecs_modules.astronomy import find_lst, tel_str
+from gtecs.tecs_modules.observing import wait_for_telescope, goto
 
 
 def run():
     print('Start of Night Phase 2')
     cmd('mnt info')
-    cmd('mnt unpark')
+    print('Setting RA to LST+4')
+    ra_deg = Longitude(u.hour * (4 + find_lst(Time.now()))).deg
+
+    goto(ra_deg, 23)
+    wait_for_telescope(240)
+
     time.sleep(5)
     cmd('mnt info')
 
