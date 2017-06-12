0bc8d3ff050e841701c0d4d35c27e9038a9d653e
Martin Dyer <martin.dyer@sheffield.ac.uk>
2017-06-12 11:34:15 +0100

    Increase timeout for fetching images

--- gtecs/daemons/cam_daemon.py
+++ gtecs/daemons/cam_daemon.py
@@ -421,7 +421,7 @@ class CamDaemon(HardwareDaemon):
     def _image_fetch(self, tel):
         intf, HW = self.tel_dict[tel]
         fli = Pyro4.Proxy(params.FLI_INTERFACES[intf]['ADDRESS'])
-        fli._pyroTimeout = params.PROXY_TIMEOUT
+        fli._pyroTimeout = 99 #params.PROXY_TIMEOUT
         try:
             future_image = fli.fetch_exposure(HW)
         except:
