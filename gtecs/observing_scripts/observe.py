0ce084021ddbdaa54b756b430bf4ef6af7a04d2d
Martin Dyer <martin.dyer@sheffield.ac.uk>
2017-06-14 10:29:43 +0100

    Make sure everywhere uses new ExposureSet name

--- gtecs/observing_scripts/observe.py
+++ gtecs/observing_scripts/observe.py
@@ -44,9 +44,9 @@ def get_exq_commands(pointingID):
     commands = []
     with open_session() as session:
         pointing = get_pointing_by_id(session, pointingID)
-        for exposure in pointing.exposures:
+        for exposure_set in pointing.exposure_sets:
             keywords = pointing.__dict__.copy()
-            keywords.update(exposure.__dict__)
+            keywords.update(exposure_set.__dict__)
             commands.append(command_template.format(**keywords))
     return commands
 
