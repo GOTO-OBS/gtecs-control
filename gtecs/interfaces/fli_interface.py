c5f62ec94cce03b4c53844121ab6c2af67ed8ebd
Martin Dyer <martin.dyer@sheffield.ac.uk>
2017-06-30 22:24:35 +0100

    Unsatisfying bodge for interface hosts

--- gtecs/interfaces/fli_interface.py
+++ gtecs/interfaces/fli_interface.py
@@ -253,6 +253,11 @@ def start():
     '''
     # find which interface this is
     hostname = socket.gethostname()
+    if hostname == 'nuc-east.warwick.ac.uk':
+        hostname = '10.2.6.14'
+    elif hostname == 'nuc-west.warwick.ac.uk':
+        hostname = '10.2.6.16'
+
     intf = misc.find_interface_ID(hostname)
 
     host = params.FLI_INTERFACES[intf]['HOST']
