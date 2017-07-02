886cff4bcfdb6b209a2f9ab56f5d1e5d39a2d742
Martin Dyer <martin.dyer@sheffield.ac.uk>
2017-07-02 16:57:03 +0100

    Fix error in header mount info

--- gtecs/daemons/cam_daemon.py
+++ gtecs/daemons/cam_daemon.py
@@ -724,7 +724,7 @@ class CamDaemon(HardwareDaemon):
             mnt_ra_str = '{:+03.0f}:{:02.0f}:{:04.1f}'.format(ra_h, ra_m, ra_s)
 
             mnt_dec = info['mount_dec']
-            dec_m, dec_s = divmod(abs(targ_dec)*3600,60)
+            dec_m, dec_s = divmod(abs(mnt_dec)*3600,60)
             dec_d, dec_m = divmod(dec_m,60)
             if mnt_dec < 0: dec_d = -dec_d
             mnt_dec_str = '{:+03.0f}:{:02.0f}:{:04.1f}'.format(dec_d, dec_m, dec_s)
