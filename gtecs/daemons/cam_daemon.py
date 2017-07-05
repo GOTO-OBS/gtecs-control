f521cdbd33404cbcca6359e521b20b8483d056d3
Martin Dyer <martin.dyer@sheffield.ac.uk>
2017-07-05 22:48:51 +0100

    Clear USBCamera image queue before exposing

--- gtecs/daemons/cam_daemon.py
+++ gtecs/daemons/cam_daemon.py
@@ -144,6 +144,7 @@ class CamDaemon(HardwareDaemon):
                     fli = fli_proxies[intf]
                     try:
                         fli._pyroReconnect()
+                        fli.clear_exposure_queue()
                         c = fli.set_camera_area(0, 0, 8304, 6220, HW)
                         if c: self.logfile.info(c)
                         c = fli.set_exposure(exptime_ms,frametype,HW)
