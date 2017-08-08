25cfb115da2cc349b7d27d8cb15dd19119074b66
Martin Dyer <martin.dyer@sheffield.ac.uk>
2017-08-08 17:29:05 +0100

    Now we can raise errors in the control scripts

--- gtecs/tecs_modules/misc.py
+++ gtecs/tecs_modules/misc.py
@@ -351,6 +351,11 @@ class MultipleDaemonError(Exception):
     pass
 
 
+class InputError(Exception):
+    '''To be used if an input command or arguments aren't valid.'''
+    pass
+
+
 class HardwareStatusError(Exception):
     '''To be used if a command isn't possible due to the hardware status.
     e.g. trying to start an exposure when the cameras are already exposing
