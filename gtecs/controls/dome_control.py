aed0c21df4e8807ec40fd0f1e9cd4c974346d8fe
Martin Dyer <martin.dyer@sheffield.ac.uk>
2018-04-18 18:51:39 +0100

    Add some log lines for heartbeat

--- gtecs/controls/dome_control.py
+++ gtecs/controls/dome_control.py
@@ -486,6 +486,7 @@ class AstroHavenDome:
             if self.heartbeat_serial.in_waiting:
                 out = self.heartbeat_serial.read(self.heartbeat_serial.in_waiting)
                 x = out[-1]
+                print('heartbeat says:{}'.format(x))
                 self._parse_heartbeat_status(x)
             return 0
         except:
@@ -515,6 +516,7 @@ class AstroHavenDome:
                 # send the heartbeat time to the serial port
                 t = bytes([heartbeat_timeout * 2])  # takes .5 second intervals
                 l = self.heartbeat_serial.write(t)
+                print('send byte to heartbeat')
 
             time.sleep(0.5)
 
