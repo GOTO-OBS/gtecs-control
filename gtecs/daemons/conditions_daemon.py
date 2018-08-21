6867d91cf56955d9f2cb7799c7c0d696caef7497
Martin Dyer <martin.dyer@sheffield.ac.uk>
2018-08-21 13:52:46 +0100

    Format conditions update time correctly

--- gtecs/daemons/conditions_daemon.py
+++ gtecs/daemons/conditions_daemon.py
@@ -337,7 +337,7 @@ class ConditionsDaemon(BaseDaemon):
         # ~~~~~~~~~~~~~~
         # Write data to the conditions flags file
         data = self.flags.copy()
-        data['update_time'] = update_time
+        data['update_time'] = Time(update_time, format='unix').iso
         flags_file = params.CONFIG_PATH + 'conditions_flags'
         with open(flags_file, 'w') as f:
             json.dump(data, f)
