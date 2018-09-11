c48aeeba56a7065fac5839415f11fa9e660154b5
Martin Dyer <martin.dyer@sheffield.ac.uk>
2018-09-11 10:30:57 +0100

    Add a command to pause and resume the alert listener

--- gtecs/daemons/sentinel_daemon.py
+++ gtecs/daemons/sentinel_daemon.py
@@ -24,7 +24,7 @@ class SentinelDaemon(BaseDaemon):
         super().__init__('sentinel')
 
         # sentinel variables
-        self.check_period = 5  # TODO: put into params
+        self.listening = True
 
         # start control thread
         t = threading.Thread(target=self._control_thread)
@@ -65,64 +65,70 @@ class SentinelDaemon(BaseDaemon):
         https://github.com/lpsinger/pygcn/blob/master/gcn/voeventclient.py
 
         """
-        self.log.info('Alert Listener thread started')
+        self.log.info('Alert listener thread started')
 
         # Generate a handler function using the logger
         handler = Handler(self.log).get_handler()
 
         # This first while loop means the socket will be recreated if it closes.
         while self.running:
-            # Create the socket
-            vo_socket = pygcn._open_socket(params.VOSERVER_HOST, params.VOSERVER_PORT,
-                                           log=self.log,
-                                           iamalive_timeout=90,
-                                           max_reconnect_timeout=32)
+            # Only listen if self.listening is True
+            if self.listening:
+                # Create the socket
+                vo_socket = pygcn._open_socket(params.VOSERVER_HOST, params.VOSERVER_PORT,
+                                               log=self.log,
+                                               iamalive_timeout=90,
+                                               max_reconnect_timeout=8)
 
-            # Create a simple listen function
-            def _listen(vo_socket, handler):
+                # Create a simple listen function
+                def _listen(vo_socket, handler):
+                    try:
+                        while True:
+                            pygcn._ingest_packet(vo_socket, params.LOCAL_IVO, handler, self.log)
+                    except socket.timeout:
+                        self.log.warning('socket timed out')
+                    except socket.error:
+                        self.log.warning('socket error')
+                    except XMLSyntaxError:
+                        self.log.error('XML syntax error')
+                        self.log.debug('', exc_info=True)
+                    except Exception as err:
+                        self.log.error('Error in alert listener')
+                        self.log.debug('', exc_info=True)
+
+                # launch the listener within a new thread
+                listener = threading.Thread(target=_listen, args=(vo_socket, handler))
+                listener.daemon = True
+                listener.start()
+
+                # This second loop will monitor the thread
+                while self.running and self.listening:
+                    if listener.is_alive():
+                        time.sleep(1)
+                    else:
+                        self.log.error('Alert listener failed')
+                        break
+
+                # Either the listener failed or listening or running have been set to False
+                # Close the socket nicely
                 try:
-                    while True:
-                        pygcn._ingest_packet(vo_socket, params.LOCAL_IVO, handler, self.log)
-                except socket.timeout:
-                    self.log.warning('socket timed out')
+                    vo_socket.shutdown(socket.SHUT_RDWR)
                 except socket.error:
-                    self.log.warning('socket error')
-                except XMLSyntaxError:
-                    self.log.error('XML syntax error')
+                    self.log.error('Could not shut down socket')
                     self.log.debug('', exc_info=True)
-                except Exception as err:
-                    self.log.error('Error in alert listener')
+                try:
+                    vo_socket.close()
+                except socket.error:
+                    self.log.error('Could not close socket')
                     self.log.debug('', exc_info=True)
-
-            # launch the listener within a new thread
-            listner = threading.Thread(target=_listen, args=(vo_socket, handler))
-            listner.daemon = True
-            listner.start()
-
-            # This second loop will monitor the thread
-            while self.running:
-                if listner.is_alive():
-                    time.sleep(1)
                 else:
-                    print('Alert listener failed')
-                    break
+                    self.log.info('closed socket connection')
 
-            # Either the listener failed or we're no longer running
-            # Close the socket nicely
-            try:
-                vo_socket.shutdown(socket.SHUT_RDWR)
-            except socket.error:
-                self.log.error('Could not shut down socket')
-                self.log.debug('', exc_info=True)
-            try:
-                vo_socket.close()
-            except socket.error:
-                self.log.error('Could not close socket')
-                self.log.debug('', exc_info=True)
             else:
-                self.log.info('closed socket connection')
+                self.log.warning('Alert listener paused')
+                time.sleep(2)
 
-        self.log.info('Alert Listener thread stopped')
+        self.log.info('Alert listener thread stopped')
         return
 
     # Internal functions
@@ -143,14 +149,23 @@ class SentinelDaemon(BaseDaemon):
         self._check_errors()
 
     # Control functions
-    def check_queue(self):
-        """Check the current queue for the best pointing to do."""
-        next_pointing = self.info['next_pointing']
-        if next_pointing is not None:
-            self.log.info('Scheduler returns: pointing ID {}'.format(next_pointing.pointing_id))
-        else:
-            self.log.info('Scheduler returns: None')
-        return next_pointing
+    def pause_listener(self):
+        """Pause the alert listener."""
+        if not self.listening:
+            return 'Alert listener already pasued'
+
+        self.log.info('Pausing alert listener')
+        self.listening = False
+        return 'Alert listener paused'
+
+    def resume_listener(self):
+        """Pause the alert listener."""
+        if self.listening:
+            return 'Alert listener already running'
+
+        self.log.info('Resuming alert listener')
+        self.listening = True
+        return 'Alert listener resumed'
 
 
 if __name__ == "__main__":
