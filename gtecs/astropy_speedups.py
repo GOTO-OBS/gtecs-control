453fa37f04996dc894bd58070f98c86d5d94036f
Stuart Littlefair <s.littlefair@shef.ac.uk>
2019-01-25 13:23:39 +0000

    erfa now returns structured arrays

--- gtecs/astropy_speedups.py
+++ gtecs/astropy_speedups.py
@@ -71,20 +71,18 @@ def get_astrom(frame, tcode, precision=600):
     if tcode in ['apci', 'apcs']:
         # find the position and velocity of earth
         if mjd_resolution:
-            (
-                earth_pv_support, earth_heliocentric_support
-                ) = prepare_earth_position_vel(obstime_support)
+            earth_pv_support, earth_heliocentric_support = prepare_earth_position_vel(obstime_support)
             # do interpolation
-            earth_pv = np.empty(obstime.shape + (2, 3,))
+            earth_pv = np.empty(obstime.shape, dtype=earth_pv_support.dtype)
             earth_heliocentric = np.empty(obstime.shape + (3,))
             for dim in range(3):
-                for ipv in range(2):
-                    earth_pv[..., ipv, dim] = np.interp(
-                        obstime.mjd, obstime_support.mjd, earth_pv_support[:, ipv, dim]
-                        )
+                for pv in ('p', 'v'):
+                    earth_pv[pv][..., dim] = np.interp(
+                        obstime.mjd, obstime_support.mjd, earth_pv_support[pv][..., dim]
+                    )
                 earth_heliocentric[..., dim] = np.interp(
                     obstime.mjd, obstime_support.mjd, earth_heliocentric_support[:, dim]
-                    )
+                )
 
         else:
             earth_pv, earth_heliocentric = prepare_earth_position_vel(obstime)
@@ -129,10 +127,10 @@ def get_astrom(frame, tcode, precision=600):
         # get the position and velocity arrays for the observatory.  Need to
         # have xyz in last dimension, and pos/vel in one-but-last.
         # (Note could use np.stack once our minimum numpy version is >=1.10.)
-        pv = np.concatenate(
-                (frame.obsgeoloc.get_xyz(xyz_axis=-1).value[..., np.newaxis, :],
-                 frame.obsgeovel.get_xyz(xyz_axis=-1).value[..., np.newaxis, :]),
-                axis=-2)
+        pv = erfa.pav2pv(
+            frame.obsgeoloc.get_xyz(xyz_axis=-1).to_value(u.m),
+            frame.obsgeovel.get_xyz(xyz_axis=-1).to_value(u.m/u.s)
+        )
         astrom = erfa.apcs(jd1_tt, jd2_tt, pv, earth_pv, earth_heliocentric)
 
     elif tcode == 'apci13':
