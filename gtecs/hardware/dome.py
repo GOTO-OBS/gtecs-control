626a45943a5f82832ed72f4a98b31b0a1174e01d
Martin Dyer <martin.dyer@sheffield.ac.uk>
2019-02-17 12:07:07 +0000

    Can't set autoclose like that now

--- gtecs/hardware/dome.py
+++ gtecs/hardware/dome.py
@@ -536,11 +536,6 @@ class AstroHavenDome(object):
                     self.heartbeat_serial.write(0)
             else:
                 if self.heartbeat_status == 'closed':
-                    # reenable autoclose if it's not already
-                    status = flags.Status()
-                    if not status.autoclose:
-                        print('closed due to heartbeat, reenable autoclose')
-                        status.autoclose = True
                     # send a 0 to reset it
                     # print('sending reset value')
                     self.heartbeat_serial.write(chr(0).encode('ascii'))
