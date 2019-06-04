79cd9674c61bb36f35082d524f3b50be170296a5
Martin Dyer <martin.dyer@sheffield.ac.uk>
2019-06-04 14:14:01 +0100

    Start by loading the previous dict

--- gtecs/daemons/conditions_daemon.py
+++ gtecs/daemons/conditions_daemon.py
@@ -13,6 +13,7 @@ from gtecs import misc
 from gtecs import params
 from gtecs.astronomy import get_sunalt
 from gtecs.daemons import BaseDaemon
+from gtecs.flags import load_json
 from gtecs.slack import send_slack_msg
 
 import numpy as np
@@ -45,7 +46,12 @@ class ConditionsDaemon(BaseDaemon):
                                     ]
         self.flag_names = self.info_flag_names + self.normal_flag_names + self.critical_flag_names
 
-        self.flags = {flag: 2 for flag in self.flag_names}
+        try:
+            flags_file = os.path.join(params.FILE_PATH, 'conditions_flags')
+            old_dict = load_json(flags_file)
+            self.flags = {flag: old_dict[flag] for flag in self.flag_names}
+        except Exception:
+            self.flags = {flag: 2 for flag in self.flag_names}
         self.update_time = {flag: 0 for flag in self.flag_names}
 
         # start control thread
