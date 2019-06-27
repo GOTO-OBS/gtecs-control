51301c1b357a1a28a97b6491e9bd9046cf5e8ab8
Martin Dyer <martin.dyer@sheffield.ac.uk>
2019-06-27 15:12:06 +0100

    Add fake Event class so we can simulate skymaps

--- gtecs/simulations/scripts/sim_skymap.py
+++ gtecs/simulations/scripts/sim_skymap.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Simulate a night observing a given event.
+"""Simulate a night observing a given skymap, treating it as an LVC binary-black hole.
 
 Where possible, this script uses the real G-TeCS scheduling, ObsDB database,
 GOTO-alert event handling and GOTO-tile tiling functions to mimic the real system.
@@ -12,26 +12,31 @@ import argparse
 import warnings
 
 from gotoalert.alert import event_handler
-from gotoalert.events import Event
+
+from gototile.skymap import SkyMap
 
 from gtecs import logger
 from gtecs.simulations.database import prepare_database
+from gtecs.simulations.events import FakeEvent
 from gtecs.simulations.pilot import run as run_pilot
 
 
 warnings.simplefilter("ignore", DeprecationWarning)
 
 
-def run(ivorn):
+def run(fits_path):
     """Run the simulation."""
     # Prepare the ObsDB
     prepare_database()
 
+    # Load the skymap
+    skymap = SkyMap.from_fits(fits_path)
+
     # Create the Event
-    event = Event.from_ivorn(ivorn)
+    event = FakeEvent(skymap)
 
     # Create a log file
-    log = logger.get_logger('sim_event', log_stdout=False, log_to_file=True, log_to_stdout=True)
+    log = logger.get_logger('sim_skymap', log_stdout=False, log_to_file=True, log_to_stdout=True)
 
     # Handle the event
     # This should add tiles to the observation database, using the appropriate strategy
@@ -45,11 +50,11 @@ def run(ivorn):
 
 
 if __name__ == "__main__":
-    description = 'Process an event using the fake pilot to simulate a night of observations'
+    description = 'Process a skymap using the fake pilot to simulate a night of observations'
     parser = argparse.ArgumentParser(description=description,
-                                     usage='python sim_event.py <ivorn>')
-    parser.add_argument('ivorn',
-                        help='ivorn of the event to fetch fro nthe VOEvent database')
+                                     usage='python sim_skymap.py <path>')
+    parser.add_argument('path',
+                        help='path to the FITS skymap file')
     args = parser.parse_args()
 
-    run(args.ivorn)
+    run(args.path)
