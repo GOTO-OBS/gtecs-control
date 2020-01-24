001c91736696fac97049cd578d00edf36a8ed19e
Martin Dyer <martin.dyer@sheffield.ac.uk>
2020-01-24 17:55:43 +0000

    Wait longer after power on

--- gtecs/observing_scripts/startup.py
+++ gtecs/observing_scripts/startup.py
@@ -24,14 +24,14 @@ def run():
     # Make sure the power daemon is running
     execute_command('power start')
 
-    time.sleep(10)
+    time.sleep(5)
 
     # Power on the UT hardware and mount box
     execute_command('power on cams,focs,filts')
     time.sleep(0.5)
     execute_command('power on sitech')
 
-    time.sleep(5)
+    time.sleep(10)
 
     # Make all the other daemons and interfaces are running
     execute_command('intf start')
