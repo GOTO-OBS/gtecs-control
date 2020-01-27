d459b466b3467e508a5fc4dde52656e2bb2bb772
Martin Dyer <martin.dyer@sheffield.ac.uk>
2020-01-27 22:53:24 +0000

    Try a different method

--- gtecs/observing_scripts/takeFocusRun.py
+++ gtecs/observing_scripts/takeFocusRun.py
@@ -287,9 +287,9 @@ def run(fraction, steps, num_exp, exptime, filt, no_slew, no_plot, no_confirm):
         hfds = hfds.groupby(level=0)
         fwhms = fwhms.groupby(level=0)
         data = {'median': hfds.min(),
-                'std': hfds.std() if not np.isnan(hfds.std()) else 0,
+                'std': hfds.std() if len(hfds) > 0 else 0,
                 'fwhm': fwhms.min(),
-                'fwhm_std': fwhms.std() if not np.isnan(fwhms.std()) else 0,
+                'fwhm_std': fwhms.std() if len(fwhms) > 0 else 0,
                 'pos': pd.Series(get_current_focus()),
                 }
         print('Best HFDs:', data['median'].to_dict())
