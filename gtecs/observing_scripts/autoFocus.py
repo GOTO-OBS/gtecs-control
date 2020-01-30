c6770c242415122841bec6c6249dcac51e554345
Martin Dyer <martin.dyer@sheffield.ac.uk>
2020-01-30 07:13:57 +0000

    Only move focusers we need to move

--- gtecs/observing_scripts/autoFocus.py
+++ gtecs/observing_scripts/autoFocus.py
@@ -388,8 +388,11 @@ def run(big_step, small_step, nfv, m_l, m_r, delta_x, num_exp=3, exptime=30, fil
     while np.any(hfds > nfv):
         print('Moving focus in...')
         mask = hfds > nfv
+        moving_uts = sorted(hfds.index)[mask]
+        print('UTs above near-focus position: {}'.format(','.join(moving_uts)))
         target_hfds = (0.5 * hfds).where(mask, hfds)
         new_focus = estimate_focus(target_hfds, hfds, get_focus(), m_r)
+        new_focus = {ut: new_focus.to_dict()[ut] for ut in moving_uts}
         set_focus_carefully(new_focus, orig_focus)
         print('New focus:', get_current_focus())
 
