071673caf5e5491714469df67bcf4097c75e23c5
Martin Dyer <martin.dyer@sheffield.ac.uk>
2020-05-20 16:16:15 +0100

    Add extra darks script

---
+++ gtecs/observing_scripts/takeExtraDarks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Script to take extra dark frames to test for hot pixels."""
+
+from argparse import ArgumentParser
+
+from gtecs.misc import execute_command
+from gtecs.observing import prepare_for_images, wait_for_exposure_queue
+
+
+def run(n):
+    """Take extra dark frames."""
+    print('Taking extra test dark frames.')
+
+    # make sure hardware is ready
+    prepare_for_images()
+
+    execute_command('exq dark 60 1')
+    execute_command('exq dark 600 1')
+    execute_command('exq resume')  # just in case
+
+    # estimate a deliberately pessimistic timeout
+    readout = 10
+    total_time = (60 + readout +
+                  600 + readout)
+    total_time *= 1.5
+    wait_for_exposure_queue(total_time)
+
+    print('Extra darks done')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Take extra dark frames.')
+    run()
