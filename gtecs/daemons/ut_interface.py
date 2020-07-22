2a0f6aa76e4ede2c10887fcfad438c5219f985f0
Martin Dyer <martin.dyer@sheffield.ac.uk>
2020-07-22 10:15:58 +0100

    Might as well add reason

--- gtecs/daemons/ut_interface.py
+++ gtecs/daemons/ut_interface.py
@@ -321,8 +321,7 @@ class UTInterfaceDaemon(BaseDaemon):
     def get_focuser_temp(self, temp_type, ut):
         """Return focuser internal/external temperature."""
         if not isinstance(self.focusers[ut], H400):
-            # H400s don't have temperature sensors
-            raise NotImplementedError
+            raise NotImplementedError("ASA H400s don't have temperature sensors")
         return self.focusers[ut].read_temperature(temp_type)
 
     def get_focuser_serial_number(self, ut):
