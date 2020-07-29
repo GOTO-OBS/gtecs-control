b3aedf1876cb3baa2dd4900e240fe5f942c2cf92
Martin Dyer <martin.dyer@sheffield.ac.uk>
2020-07-29 10:15:24 +0100

    Add extra function to measure FWHM (roughly)

--- gtecs/analysis.py
+++ gtecs/analysis.py
@@ -9,8 +9,8 @@ import numpy as np
 import sep
 
 
-def measure_image_hfd(data, filter_width=15, threshold=5, xslice=None, yslice=None, verbose=True):
-    """Measure the half-flux-diameter of an image.
+def extract_image_sources(data, filter_width=15, threshold=5, xslice=None, yslice=None):
+    """Extract sources from an image using `sep.extract`.
 
     Parameters
     ----------
@@ -25,8 +25,6 @@ def measure_image_hfd(data, filter_width=15, threshold=5, xslice=None, yslice=No
         slice in x axis
     yslice : `slice`, default=slice(1500, 4500)
         slice in y axis
-    verbose : bool, default=True
-        if False, supress printout
 
     Returns
     -------
@@ -60,6 +58,69 @@ def measure_image_hfd(data, filter_width=15, threshold=5, xslice=None, yslice=No
     objects = sep.extract(data, threshold, background.globalrms,
                           filter_kernel=kernel.array, clean=True)
 
+    return objects
+
+
+def measure_image_fwhm(data, filter_width=15, threshold=5, xslice=None, yslice=None, verbose=True):
+    """Measure the median FWHM of sources in an image.
+
+    Parameters
+    ----------
+    verbose : bool, default=True
+        if False, supress printout
+
+    For other parameters see `gtecs.analysis.extract_image_sources()`
+
+    Returns
+    -------
+    median : float
+        median FWHM value
+    std : float
+        standard deviation of FWHM measurements
+
+    """
+    # Extract sources
+    objects = extract_image_sources(data, filter_width, threshold, xslice, yslice)
+
+    # Calculate FWHMs
+    fwhms = 2 * np.sqrt(np.log(2) * (objects['a']**2 + objects['b']**2))
+
+    # Mask any objects with high peak counts
+    mask = objects['peak'] < 40000
+    fwhms = fwhms[mask]
+    if len(fwhms) <= 3:
+        raise ValueError('Not enough objects ({}) found for FWHM measurement'.format(len(fwhms)))
+    else:
+        if verbose:
+            print('Found {} objects with measurable FWHMs'.format(len(fwhms)))
+
+    # Get median and standard deviation over all extracted objects
+    mean_fwhm, median_fwhm, std_fwhm = sigma_clipped_stats(fwhms, sigma=2.5, maxiters=10)
+
+    return median_fwhm, std_fwhm
+
+
+def measure_image_hfd(data, filter_width=15, threshold=5, xslice=None, yslice=None, verbose=True):
+    """Measure the median half-flux-diameter of sources in an image.
+
+    Parameters
+    ----------
+    verbose : bool, default=True
+        if False, supress printout
+
+    For other parameters see `gtecs.analysis.extract_image_sources()`
+
+    Returns
+    -------
+    median : float
+        median HFD value
+    std : float
+        standard deviation of HFD measurements
+
+    """
+    # Extract sources
+    objects = extract_image_sources(data, filter_width, threshold, xslice, yslice)
+
     # Measure Half-Flux Radius to find HFDs
     hfrs, flags = sep.flux_radius(data, objects['x'], objects['y'],
                                   rmax=40 * np.ones_like(objects['x']),
@@ -70,7 +131,7 @@ def measure_image_hfd(data, filter_width=15, threshold=5, xslice=None, yslice=No
     mask = np.logical_and(flags == 0, objects['peak'] < 40000)
     hfds = hfds[mask]
     if len(hfds) <= 3:
-        raise ValueError('Not enough objects ({}) found for focus measurement'.format(len(hfds)))
+        raise ValueError('Not enough objects ({}) found for HFD measurement'.format(len(hfds)))
     else:
         if verbose:
             print('Found {} objects with measurable HFDs'.format(len(hfds)))
