43d7db8b41fd674d059165a114b637e6c5a30ed7
Martin Dyer <martin.dyer@sheffield.ac.uk>
2020-09-23 14:04:39 +0100

    Update slack module

--- gtecs/slack.py
+++ gtecs/slack.py
@@ -2,16 +2,12 @@
 
 import os
 
-from slackclient import SlackClient
+import slack
 
 from . import params
 
-BOT_TOKEN = params.SLACK_BOT_TOKEN
-BOT_NAME = params.SLACK_BOT_NAME
-CHANNEL_NAME = params.SLACK_BOT_CHANNEL
 
-
-def send_slack_msg(text, attachments=None, filepath=None):
+def send_slack_msg(text, attachments=None, filepath=None, channel=params.SLACK_BOT_CHANNEL):
     """Send a message to Slack, using the settings defined in `gtecs.params`.
 
     Parameters
@@ -27,35 +23,37 @@ def send_slack_msg(text, attachments=None, filepath=None):
         A local path to a file to be added to the message.
         NB a message can have a file OR attachments, not both.
 
+    channel : string, optional
+        The channel to post the message to.
+        Defaults to `gtecs.params.SLACK_BOT_CHANNEL`.
+
     """
     text = str(text)
     if attachments is not None and filepath is not None:
         raise ValueError("A Slack message can't have both attachments and a file.")
 
     if params.ENABLE_SLACK:
-        client = SlackClient(BOT_TOKEN)
+        client = slack.WebClient(params.SLACK_BOT_TOKEN)
         try:
             if not filepath:
-                api_call = client.api_call('chat.postMessage',
-                                           channel=CHANNEL_NAME,
-                                           username=BOT_NAME,
-                                           as_user=True,
-                                           text=text,
-                                           attachments=attachments,
-                                           )
+                api_call = client.chat_postMessage(channel=channel,
+                                                   username=params.SLACK_BOT_NAME,
+                                                   as_user=True,
+                                                   text=text,
+                                                   attachments=attachments,
+                                                   )
             else:
                 filename = os.path.basename(filepath)
                 name = os.path.splitext(filename)[0]
                 with open(filepath, 'rb') as file:
-                    api_call = client.api_call('files.upload',
-                                               channels=CHANNEL_NAME,  # Note channel(s)
-                                               username=BOT_NAME,
-                                               as_user=True,
-                                               initial_comment=text,
-                                               filename=filename,
-                                               file=file,
-                                               title=name,
-                                               )
+                    api_call = client.files_upload(channels=channel,  # Note channel(s)
+                                                   username=params.SLACK_BOT_NAME,
+                                                   as_user=True,
+                                                   initial_comment=text,
+                                                   filename=filename,
+                                                   file=file,
+                                                   title=name,
+                                                   )
             if not api_call.get('ok'):
                 raise Exception('Unable to send message')
         except Exception as err:
