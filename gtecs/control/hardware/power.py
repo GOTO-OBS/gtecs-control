a940c6910591ca3c4e9fa45a69c86fcd471e234b
Martin Dyer <martin.dyer@sheffield.ac.uk>
2021-06-21 22:46:54 +0100

    Add class for ATS

--- gtecs/control/hardware/power.py
+++ gtecs/control/hardware/power.py
@@ -360,6 +360,70 @@ class APCUPS(object):
         return out
 
 
+class APCATS(object):
+    """APC ATS power class (for AP4421 Rack Automatic Transfer Switch)."""
+
+    def __init__(self, address):
+        self.unit_type = 'ATS'
+        self.address = address
+        self.command_oids = {'STATUS': '5.1.3.0',
+                             'STATUS_A': '5.1.12.0',
+                             'STATUS_B': '5.1.13.0',
+                             'SOURCE': '5.1.2.0',
+                             }
+        self.statuses = {'1': 'ERROR',
+                         '2': 'Normal',
+                         }
+        self.sources = {'1': 'A',
+                        '2': 'B',
+                        }
+
+    def _initialise_oid_array(self, command_oid):
+        """Set up the oid array to use with snmpget and snmpset."""
+        base = '.1.3.6.1.4.1.318.1.1.8'
+        oid_arr = [base + '.' + str(command_oid)]
+        return oid_arr
+
+    def _snmpget(self, oid_arr):
+        """Get a value using snmpget."""
+        snmpget = shutil.which('snmpget')
+        if snmpget is None:
+            raise OSError('SNMP tools not installed')
+        address = self.address
+        command = [snmpget, '-v', '1', '-c', 'public', address] + oid_arr
+        output = subprocess.check_output(command).decode('ascii').split('\n')
+        status = ''
+        for i in range(len(output) - 1):
+            status += output[i].split(' ')[-1]
+        return status
+
+    def status(self):
+        """Return the current status of the ATS."""
+        oid_arr = self._initialise_oid_array(self.command_oids['STATUS'])
+        out = self._snmpget(oid_arr)
+        status = self.statuses[out]
+        return status
+
+    def source_status(self, source):
+        """Return the current status of the given source."""
+        if source == 'A':
+            oid_arr = self._initialise_oid_array(self.command_oids['STATUS_A'])
+        elif source == 'B':
+            oid_arr = self._initialise_oid_array(self.command_oids['STATUS_B'])
+        else:
+            raise ValueError('Invalid source')
+        out = self._snmpget(oid_arr)
+        status = self.statuses[out]
+        return status
+
+    def active_source(self):
+        """Return which source is currently active."""
+        oid_arr = self._initialise_oid_array(self.command_oids['SOURCE'])
+        out = self._snmpget(oid_arr)
+        source = self.sources[out]
+        return source
+
+
 class ETH8020(object):
     """Ethernet relay power class (for ETH8020, 20 ports)."""
 
