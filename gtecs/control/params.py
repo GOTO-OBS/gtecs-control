8b3b06c700ea600286fba091183a332c0ecb8b29
Martin Dyer <martin.dyer@sheffield.ac.uk>
2021-08-10 13:00:03 +0100

    Only keep the name, not the path

--- gtecs/control/params.py
+++ gtecs/control/params.py
@@ -19,7 +19,7 @@ config, CONFIG_SPEC, CONFIG_FILE = load_config('control', ['.gtecs.conf', '.cont
 ############################################################
 # Module parameters
 VERSION = get_package_version('control')
-PYTHON_EXE = sys.executable
+PYTHON_EXE = sys.executable.split('/')[-1]
 if '.' not in PYTHON_EXE:
     # Needed for remote machines, which might have different versions as default
     # (e.g. python3 might be python3.6, but we want to force python3.8)
