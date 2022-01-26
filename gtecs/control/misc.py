920be03711b983c3b6cc8111d0706c1a15373ed3
Martin Dyer <martin.dyer@sheffield.ac.uk>
2022-01-26 11:15:54 +0000

    Add utility function for IP

--- gtecs/control/misc.py
+++ gtecs/control/misc.py
@@ -152,6 +152,21 @@ class NeatCloser(object, metaclass=abc.ABCMeta):
         return
 
 
+def get_host_ip():
+    """Get the current host IP (https://stackoverflow.com/a/28950776)."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+
 def get_pid(pidname, host=None):
     """Check if a pid file exists with the given name.
 
