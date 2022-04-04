b1bc08313a171976e734705776427aab73986b69
Martin Dyer <martin.dyer@sheffield.ac.uk>
2022-04-04 15:45:30 +0100

    Add to module

--- gtecs/control/hardware/mount/__init__.py
+++ gtecs/control/hardware/mount/__init__.py
@@ -1,7 +1,8 @@
 """Mount control classes."""
 
-from .asa_sdk import DDM500
+from .asa_alpaca import DDM500
+from .asa_sdk import DDM500 as DDM500SDK
 from .asa_tcp import DDM500 as DDM500TCP
 from .sitech import SiTech
 
-__all__ = [DDM500, DDM500TCP, SiTech]
+__all__ = [DDM500, DDM500SDK, DDM500TCP, SiTech]
